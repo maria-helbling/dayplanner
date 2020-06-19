0e8c024ba1bb87b99227a3a418ccdcc9cbc183c6
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-06-18 22:57:19 -0700

    save button listener and actions

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,13 @@ let day = moment().format('Do');
 let month = moment().format('MMMM');
 let year = moment().format('YYYY')
 let hour = moment().format('hA')
-
+//variables for handling color coding
 let past = true;
 let present = false;
 
 //Check for stored tasks
-let storedTasks= JSON.parse(localStorage.getItem('taskList'))
+let storedTasks = JSON.parse(localStorage.getItem('taskList'))
+//for first time use set up local storage structure
 if (!storedTasks) {
     storedTasks = {
         0: "",
@@ -23,12 +24,15 @@ if (!storedTasks) {
         9: ""
     }
 }
+
 // day etc printed on top of page
 $('#currentDay').text(`${weekday}, ${day} ${month} ${year}`);
 
-// write all hourblocks
+// populate the planner block
 for (let i = 0; i < 9; i++) {
-    let loopHour=moment().hour(i+9).format('hA')
+
+    let loopHour = moment().hour(i + 9).format('hA')
+    //identify current hour
     if (loopHour === hour) {
         past = false;
         present = true;
@@ -48,23 +52,38 @@ for (let i = 0; i < 9; i++) {
         textSpace.addClass('past')
     } else if (present) {
         textSpace.addClass('present')
-        present=false;
+        present = false;
     } else {
         textSpace.addClass('future')
     }
     thisRow.append(textSpace)
-    //TODO: add button picture change
+    //add the save buttons depending on if the hour is already fille din
     let btn = $(`<button class="col-1 btn saveBtn" data-index="${i}">`)
-    if (storedTasks[i].length){
-        btn.append(`<i class="fas fa-lock">`)
+    if (storedTasks[i].length) {
+        btn.append(`<i class="fas fa-lock" data-index="${i}">`)
+        textSpace.prop('disabled', true)
     } else {
-        btn.append(`<i class="fas fa-unlock-alt">`)
+        btn.append(`<i class="fas fa-unlock-alt" data-index="${i}">`)
     }
     thisRow.append(btn)
 }
 
-
-
-//TODO: read to-dos from storage
-//TODO: read input from user
-//TODO: save to storage if button clicked
+//listen to user mouse click
+$('.saveBtn').click(function () {
+    let index = $(this).attr('data-index');
+    //choose button effect and storage option based on if a stored task is unlocked or new task is locked in
+    if (storedTasks[index]) {
+        $(`textarea[data-index="${index}"]`).prop('disabled', false);
+        storedTasks[index] = "";
+        $(`i[data-index="${index}"]`).removeClass('fa-lock').addClass('fa-unlock-alt');
+        localStorage.setItem('taskList', JSON.stringify(storedTasks));
+    } else {
+        let txt = $(`textarea[data-index="${index}"]`)
+        if (txt.val()) { //handles the case that save is clicked on empty text area
+            storedTasks[index] = txt.val();
+            txt.prop('disabled', true);
+            $(`i[data-index="${index}"]`).removeClass('fa-unlock-alt').addClass('fa-lock');
+            localStorage.setItem('taskList', JSON.stringify(storedTasks));
+        }
+    }
+})
